601acd443dd1aa9be09f9a817fe5dad70ac1e7a7
秦思成 <464815411@qq.com>
2024-10-01 20:17:28 +0800

    增加定时功能

--- pages/script.js
+++ pages/script.js
@@ -1,65 +1,111 @@
 const form = document.getElementById('form')
 const input = document.getElementById('input')
+const title = document.getElementById('titles')
 const todosUL = document.getElementById('todos')
+const myAudio = document.getElementById('myAudio')
 
+// 上一次的定时器
+let lastInterval = null
+
+// 序列化后的todos
 const todos = JSON.parse(localStorage.getItem('todos'))
 
-if(todos) {
-    todos.forEach(todo => addTodo(todo))
+// 如果有todos，就添加到页面上
+if (todos) {
+  todos.forEach(todo => addTodo(todo))
 }
 
+// 提交表单
 form.addEventListener('submit', (e) => {
-    e.preventDefault()
-
-    addTodo()
+  e.preventDefault()
+  addTodo()
 })
 
 function addTodo(todo) {
-    let todoText = input.value
+  let todoText = input.value
 
-    if(todo) {
-        todoText = todo.text
+  if (todo) {
+    todoText = todo.text
+  }
+
+  if (todoText) {
+    const todoEl = document.createElement('li')
+    const todoSpan = document.createElement('span')
+    const todoBtn = document.createElement('button')
+
+    todoBtn.innerText = '小番茄'
+
+    todoEl.appendChild(todoSpan)
+    todoEl.appendChild(todoBtn)
+
+    todoBtn.addEventListener('click', () => {
+      form.style.display = 'none';
+      title.innerText = '25:00';
+      startCountdown(title, 25 * 60);
+    })
+
+    if (todo && todo.completed) {
+      todoEl.classList.add('completed')
     }
 
-    if(todoText) {
-        const todoEl = document.createElement('li')
-        if(todo && todo.completed) {
-            todoEl.classList.add('completed')
-        }
+    todoSpan.innerText = todoText
 
-        todoEl.innerText = todoText
+    // todoEl.addEventListener('click', () => {
+    //   todoEl.classList.toggle('completed')
+    //   updateLS()
+    // })
 
-        todoEl.addEventListener('click', () => {
-            todoEl.classList.toggle('completed')
-            updateLS()
-        }) 
+    // todoEl.addEventListener('contextmenu', (e) => {
+    //   e.preventDefault()
 
-        todoEl.addEventListener('contextmenu', (e) => {
-            e.preventDefault()
+    //   todoEl.remove()
+    //   updateLS()
+    // })
 
-            todoEl.remove()
-            updateLS()
-        }) 
+    todosUL.appendChild(todoEl)
 
-        todosUL.appendChild(todoEl)
+    input.value = ''
 
-        input.value = ''
+    // updateLS()
+  }
+}
 
-        updateLS()
+function startCountdown(element, seconds) {
+  if (lastInterval) {
+    clearInterval(lastInterval)
+  }
+
+  var intervalId = setInterval(function () {
+    seconds -= 1200;
+    var minutes = Math.floor(seconds / 60);
+    var remainingSeconds = seconds % 60;
+    element.textContent = minutes + ':' + (remainingSeconds < 10 ? '0' : '') + remainingSeconds;
+    if (seconds <= 0) {
+      clearInterval(intervalId);
+      document.getElementById('form').style.display = 'block';
+      element.innerText = 'todos';
+      myAudio.play();
+
+      new window.Notification('时间到', {
+        body: '您的番茄钟时间到了！',
+      });
     }
+  }, 1000);
+
+  lastInterval = intervalId
 }
 
 function updateLS() {
-    todosEl = document.querySelectorAll('li')
+  todosEl = document.querySelectorAll('li')
 
-    const todos = []
+  const todos = []
 
-    todosEl.forEach(todoEl => {
-        todos.push({
-            text: todoEl.innerText,
-            completed: todoEl.classList.contains('completed')
-        })
+  todosEl.forEach(todoEl => {
+    todos.push({
+      text: todoEl.innerText,
+      completed: todoEl.classList.contains('completed')
     })
+  })
 
-    localStorage.setItem('todos', JSON.stringify(todos))
+  localStorage.setItem('todos', JSON.stringify(todos))
 }
