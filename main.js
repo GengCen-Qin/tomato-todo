b2f9e02342289d11e0acab5a059ebc7873088071
秦思成 <464815411@qq.com>
2024-10-01 15:30:31 +0800

    添加启动类和主页面基本代码

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
-console.log('start---')
+const { app, BrowserWindow } = require('electron')
+
+// 当app准备好就执行创建一个窗口
+app.on('ready', () => {
+  // 创建窗口(下面的配置在上面讲过了,这里就删掉了,只留下三个)
+  let win = new BrowserWindow({
+    width: 800,
+    height: 600, //长宽
+    autoHideMenuBar: true, //只保留标题，不保留其他的选项卡部分,也是隐藏菜单栏意思
+  })
+
+  win.on('close', () => {
+    // 从性能考虑,应该释放窗体这个变量,删除窗体引用
+    win = null
+  })
+
+  win.loadFile('./pages/index.html') // loadFile就是加载本地页面的,loadURL加载的是在线链接
+})
